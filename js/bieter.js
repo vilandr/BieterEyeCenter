97aba80a0b4d926d0cb19de498dd65bee570c24b
Ryan Viland <rviland@gmail.com>
2015-08-26 11:29:46 -0500

    more responsive home page plus different navigation

--- js/bieter.js
+++ js/bieter.js
@@ -1,57 +1,65 @@
-var quote1 = '"We would not go anywhere else."<br>-Isabella M.';
-var quote2 = '"Loved my visit, EVERYONE is very friendly and extremely helpful and knowledgeable. Would definately recommend!"<br>-Cheryl M.';
-var quote3 = '"An absolute pleasurable experience. A great office with professional staff that treats you like a true friend."<br>-Dan D.';
-var quote4 = '"Thank you Dr.Beiter for seeing me quickly and retrieving the contact lens I couldn\'t \"find\" in my eye."<br>Paulette H.';
-var quote5 = '"Thank you for your professional, friendly and exceptional service!!"<br>-Lynn S.';
-var quote6 = '"I have always been pleased with the service and everyone who works there. They are exceptional."<br>-Louise B.';
-var quote7 = '"Everyone was so helpful and kind! Even though it was my first time there, I felt like we were all friends. Thank you!"<br>-Trudence T.';
-var quote8 = '"Your staff is so friendly and professional. I really appreciate all of the time that Dr. Rogers took to talk to me about my dry eyes. Thank you!"<br>-Jeffry S.';
-var quote9 = '"The staff at Bieter Eye Care is always very helpful, especially Tracy! Thank you.<br>-Mary S.';
-var quoteArray = [quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8, quote9];
+
+var quote1 = '"We would not go anywhere else."<br>-Isabella M.',
+	 quote2 = '"Loved my visit, EVERYONE is very friendly and extremely helpful and knowledgeable. Would definately recommend!"<br>-Cheryl M.',
+	 quote3 = '"An absolute pleasurable experience. A great office with professional staff that treats you like a true friend."<br>-Dan D.',
+	 quote4 = '"Thank you Dr.Beiter for seeing me quickly and retrieving the contact lens I couldn\'t \"find\" in my eye."<br>Paulette H.',
+	 quote5 = '"Thank you for your professional, friendly and exceptional service!!"<br>-Lynn S.',
+	 quote6 = '"I have always been pleased with the service and everyone who works there. They are exceptional."<br>-Louise B.',
+	 quote7 = '"Everyone was so helpful and kind! Even though it was my first time there, I felt like we were all friends. Thank you!"<br>-Trudence T.',
+	 quote8 = '"Your staff is so friendly and professional. I really appreciate all of the time that Dr. Rogers took to talk to me about my dry eyes. Thank you!"<br>-Jeffry S.',
+	 quote9 = '"The staff at Bieter Eye Care is always very helpful, especially Tracy! Thank you.<br>-Mary S.',
+	 quoteArray = [quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8, quote9];
 
 document.addEventListener("DOMContentLoaded", function() {
 	// QUOTE BANNERS
+	
+	if (window.innerWidth > 991) {
+		callLoop();
+	} else {
+		console.log("Window width is less than 992 pixels therefore the quote banners are set to display: none.");
+	}
+	var index = Math.round(Math.random() * (quoteArray.length - 1));
 
-	// var index = Math.round(Math.random() * (quoteArray.length - 1));
+	var quoteLeft = document.getElementById('quoteTextLeft');
+	quoteLeft.innerHTML = quoteArray[index];
 
-	// var quoteLeft = document.getElementById('quoteLeft');
-	// quoteLeft.innerHTML = quoteArray[index];
+	var removedQuote = quoteArray.splice(index, 1);
 
-	// quoteArray.splice(index, 1);
+	var index = Math.round(Math.random() * (quoteArray.length - 1));
 
-	// var index = Math.round(Math.random() * (quoteArray.length - 1));
+	var quoteRight = document.getElementById('quoteTextRight');
+	quoteRight.innerHTML = quoteArray[index];
 
-	// var quoteRight = document.getElementById('quoteRight');
-	// quoteRight.innerHTML = quoteArray[index];
-
-	// quoteArray.splice(index, 1);
-
-	// var quoteIndex = Math.round(Math.random() * (quoteArray.length - 1));
-
+	quoteArray.push(removedQuote);
 
 	//QUOTE BAR LOOP
 
-	var arrayIndex = 1;	
+	var arrayIndex = 0;	
 
 	function callLoop() {
 		setTimeout(function() {
-		loop();
-	}, 10000);
+		Loop();
+	}, 15000);
 	}
-	function loop() {
-		var quoteBar = document.getElementById('quoteBar');
-		quoteBar.innerHTML = quoteArray[arrayIndex];
+	function Loop() {
+		if (arrayIndex > 8) {
+			arrayIndex = 0;
+		} 
+		var quoteLeft = document.getElementById('quoteTextLeft');
+		quoteLeft.innerHTML = quoteArray[arrayIndex];
 		console.log(arrayIndex);
 		
-		if (arrayIndex == 8) {
-			arrayIndex = 0;
-			callLoop();
-		} else {
 		arrayIndex++;
-		callLoop();
-	}
+
+		if (arrayIndex > 8) {
+			arrayIndex = 0;
+		}
+		var quoteRight = document.getElementById('quoteTextRight');
+		quoteRight.innerHTML = quoteArray[arrayIndex];
+		console.log(arrayIndex);
+		
+		callLoop();	
 }
-	callLoop();
 
 	// $("ul#quoteBar").cycle({ 
 	// 	fx:'scrollUp',
